9dd2c5fe6138c3219d6a162246fcdccbbfabb1d8
VadymGor <07tennis@gmail.com>
2020-04-28 12:52:42 +0200

    Adding a DataSource

--- Taskly/Taskly/TasksController.swift
+++ Taskly/Taskly/TasksController.swift
@@ -11,3 +11,18 @@ import UIKit
 class TasksController: UITableViewController {
     
 }
+
+// MARK: - DataSource
+
+extension TasksController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Task"
+        return cell
+    }
+}
+
