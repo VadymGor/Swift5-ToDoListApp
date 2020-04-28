8c94e4a203cdf9f07a5e71cb2b59409a8b23a662
VadymGor <07tennis@gmail.com>
2020-04-28 09:43:21 +0200

    first commit

---
+++ Taskly/Taskly/TasksController.swift
@@ -0,0 +1,13 @@
+//
+//  TasksController.swift
+//  Taskly
+//
+//  Created by Vadym Horovyi on 28/04/2020.
+//  Copyright © 2020 Vadym Horovyi. All rights reserved.
+//
+
+import UIKit
+
+class TasksController: UITableViewController {
+    
+}
